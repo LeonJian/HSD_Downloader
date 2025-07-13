fb1a17e57317b6f0cb88416a035c28b866ed6115
Leon Jian <jianningyuan@outlook.com>
2025-07-14 02:30:21 +0800

    Feature 基本实现输入时间输出时间列表的功能。但是输出的时间列表可能为空（如果开始结束时间都不包含任何一个整十数时间的话） bug暂时没有发现

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
-use chrono::{NaiveDateTime, Utc};
+use chrono::{Duration, NaiveDateTime, Timelike, Utc};
 use std::fmt::Formatter;
 use std::{fmt, io};
 
 static DATE_FMT: &str = r#"%Y-%m-%d %H:%M:%S"#;
+static TIME_STEP: i64 = 10;
 
 fn main() {
     let version = env!("CARGO_PKG_VERSION");
@@ -17,16 +18,39 @@ fn main() {
         current_time.format(DATE_FMT).to_string()
     );
 
+    let current_time = current_time.naive_utc();
+
     let download_period = input_time();
 
-    match download_period {
+    let download_period = match download_period {
         Some(download_period) => {
-            println!("Download Period: {}", download_period);
+            if download_period.start_time <= download_period.end_time {
+                if download_period.start_time > current_time
+                    || download_period.end_time > current_time
+                {
+                    panic!("Input time is greater than current time");
+                }
+
+                println!("Download Period: {}", download_period);
+                download_period
+            } else {
+                panic!("End time is earlier than start time");
+            }
         }
         None => {
-            println!("No download period");
+            // println!("No download period");
+            panic!("No download period")
         }
-    }
+    };
+
+    let download_time_list = match generate_download_time_list(&download_period) {
+        Ok(download_time_list) => download_time_list,
+        Err(e) => {
+            panic!("Error generating download time list: {}", e);
+        }
+    };
+
+    println!("Download Time List: {:?}", download_time_list);
 }
 
 struct DownloadTime {
@@ -69,13 +93,16 @@ fn return_naive_date_time() -> Option<NaiveDateTime> {
 }
 
 fn input_time() -> Option<DownloadTime> {
-    println!("Input download start time: ({})", DATE_FMT);
+    println!("Input download start time(UTC Time): ({})", DATE_FMT);
     let start_time = match return_naive_date_time() {
         Some(naive_date_time) => naive_date_time,
         None => return None,
     };
 
-    println!("Input download end time: ({})", DATE_FMT);
+    println!(
+        "Input download end time(UTC Time): ({})(Use start time instead if input nothing.)",
+        DATE_FMT
+    );
     let end_time = return_naive_date_time().unwrap_or_else(|| start_time); // if end_time is nothing, we will use the start time.
 
     let download_period = DownloadTime {
@@ -83,4 +110,62 @@ fn input_time() -> Option<DownloadTime> {
         end_time,
     };
     Some(download_period)
+}
+
+fn generate_download_time_list(
+    original_time_period: &DownloadTime,
+) -> Result<Vec<NaiveDateTime>, &str> {
+    let mut start_time = original_time_period.start_time;
+    let mut start_min = start_time
+        .format("%M")
+        .to_string()
+        .parse::<u8>()
+        .expect("Failed to parse start time.");
+
+    const MAX_COUNT: u8 = 10;
+    let mut count: u8 = 0;
+    let correct_start_min = loop {
+        if count >= MAX_COUNT {
+            break None;
+        } else if start_min % 10 == 0 {
+            break Some(strip_seconds(start_time));
+        } else {
+            start_time = start_time + Duration::minutes(1);
+            start_min += 1;
+            count += 1
+        }
+    };
+
+    match correct_start_min {
+        Some(correct_start_min) => {
+            let download_time_list: Vec<NaiveDateTime> =
+                generate_ten_minute_intervals(correct_start_min, original_time_period.end_time);
+            Ok(download_time_list)
+        }
+        None => Err("Failed to generate download time list."),
+    }
+}
+
+fn strip_seconds(dt: NaiveDateTime) -> NaiveDateTime {
+    dt.with_second(0)
+        .and_then(|dt| dt.with_nanosecond(0))
+        .unwrap()
+}
+
+fn generate_ten_minute_intervals(start: NaiveDateTime, end: NaiveDateTime) -> Vec<NaiveDateTime> {
+    // 每 10 分钟一个间隔
+    let step = Duration::minutes(TIME_STEP);
+    // 计算总间隔数
+    let total_minutes = (end - start).num_minutes();
+    let count = (total_minutes / TIME_STEP) + 1; // 包含两端
+
+    let mut times = Vec::with_capacity(count as usize);
+
+    let mut current = start;
+    while current <= end {
+        times.push(current);
+        current += step;
+    }
+
+    times
 }
